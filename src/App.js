8aa594b060a90a3ea62f6236e23abf2faf3ea971
Mahmoud Abdallah <mah64725@gmail.com>
2022-08-08 17:07:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Box } from "@mui/material";
+import Countdown from "./Countdown.js";
+import Footer from './components/Footer'
+import ReactPlayer from 'react-player'
 
 function App() {
+
+  // calculate current year
+  const currentDate = new Date();
+  const year = currentDate.getMonth() === 11 && currentDate.getDate() > 23 ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center'
+      }}>
+        <Typography sx={{ mt: 3 }} variant="h4">Military Service Countdown</Typography>
+        <Countdown date={`${year}-12-01T16:00:00`} />
+      </Box>
+      {/* عبارات لمن لم يلتحقوا بالخدمه العسكريه */}
+      <Box sx={{m: 4}}>
+        <Typography textAlign='center' variant='h4' >رسالة لل مخدمش جيش</Typography>
+        <div className="player">
+          <ReactPlayer url='https://youtu.be/1QH0BeiyBzA' />
+        </div>
+      </Box>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const Footer = () => {
+    return (
+        <Box sx={{display: 'flex', flexDirection: 'column', textAlign: 'center'}}>
+            <p className='text-marginBottom'>© {new Date().getFullYear()}{'.'} <a className="copyrightLink" href="https://www.linkedin.com/in/geekahmed/">Ahmed Mostafa</a> | <a className="copyrightLink" href="https://www.linkedin.com/in/mahmoud-abdallah-hassan-942227185/">Mahmoud Abdallah</a></p>
+            <p className='text-marginTop'>All rights reserved.</p>
+        </Box>
+    )
+}
+
+export default Footer
