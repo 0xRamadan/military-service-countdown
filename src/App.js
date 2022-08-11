1c99ca2279ba39eddd19773c9d896c78f9ad63a7
Mahmoud Abdallah <mah64725@gmail.com>
2022-08-11 12:02:50 +0200

    add year field to countdown

--- src/App.js
+++ src/App.js
@@ -11,16 +11,29 @@ function App() {
 
   return (
     <div className="App">
+      <Typography sx={{ m: 3 }} variant="h4" fontWeight='bold' textAlign='center'>Military Service Countdown</Typography>
+      {/* دفعة أولي */}
       <Box sx={{
         display: 'flex',
         flexDirection: 'column',
         textAlign: 'center'
       }}>
-        <Typography sx={{ mt: 3 }} variant="h4">Military Service Countdown</Typography>
-        <Countdown date={`${year}-12-01T16:00:00`} />
+        <Typography variant='h5' fontWeight='bold'>دفعة أولي</Typography>
+        <Countdown date={`2023-12-01T16:00:00`} />
+      </Box>
+      {/* دفعة تانيه */}
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          textAlign: 'center'
+        }}
+      >
+        <Typography variant='h5' fontWeight='bold' >دفعة تانية</Typography>
+        <Countdown date={`2024-03-01T16:00:00`} />
       </Box>
       {/* عبارات لمن لم يلتحقوا بالخدمه العسكريه */}
-      <Box sx={{m: 4}}>
+      <Box sx={{ m: 4 }}>
         <Typography textAlign='center' variant='h4' >رسالة لل مخدمش جيش</Typography>
         <div className="player">
           <ReactPlayer url='https://youtu.be/1QH0BeiyBzA' />
